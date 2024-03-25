8bc2542c02a115e77167a9c1e26e180718d880f8
Ümit Özay <umitozay95@gmail.com>
2024-03-25 14:48:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once dirname(__FILE__) . "/Mobile_Detect.php";
 $detect = new Mobile_Detect;
 
 if (($detect->isMobile() || $detect->isTablet()) && !$detect->is('Bot')) {
-    echo '<div class="doc"><iframe style="width:100%;height:100%;min-width:100%;" frameborder="0" marginwidth="0" marginheight="0" src="https://www.maksatokey.com/yenitema/okey.html"></iframe></div>';
+    echo 'mobile echo'
     exit;
 }
 
